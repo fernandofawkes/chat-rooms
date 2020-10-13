18e5a487d4449c174f57160de961efb49191337b
Fernando Malaman <fernandomalaman@brq.com>
2020-10-13 16:17:48 -0300

    simple socket communication

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const socket = require('socket.io');
 
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+
 const port = process.env.PORT || '3001';
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log('User joined room ' + data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
